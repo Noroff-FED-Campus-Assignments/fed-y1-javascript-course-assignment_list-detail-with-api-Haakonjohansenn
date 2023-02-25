db8c1d65b9d2cf71a67c8ad28178d900e9efd79d
Haakonjohansenn <111745800+Haakonjohansenn@users.noreply.github.com>
2023-02-25 16:00:48 +0100

    Made website for course assignment javascipt

--- src/js/app.js
+++ src/js/app.js
@@ -51,3 +51,57 @@ https://github.com/S3ak/fed-javascript1-api-calls/blob/main/examples/games.html#
  * @example https://github.com/S3ak/fed-javascript1-api-calls/blob/main/src/js/detail.js#L36
  * @param {item} item The object with properties from the fetched JSON data.
  */
+import errorMessage from "./components/error.js";
+
+const resultsContainer = document.querySelector("#js-list-container");
+const search = document.querySelector("#search-bar");
+
+const url = "https://thronesapi.com/api/v2/characters";
+
+fetchCharacters();
+
+search.addEventListener("keyup", function(e){
+  const searchResult = e.target.value.trim().toLowerCase();
+  fetchCharacters(searchResult);
+});
+
+async function fetchCharacters(searchValue = "") {
+  try {
+
+    for (let i = 0; i < 100; i++) {
+      resultsContainer.innerHTML += `<div class="skeleton-card">
+                                     <div class="skeleton-loader skeleton-image" src=""></div>
+                                     <div class="skeleton-loader skeleton-fullname"><p></p></div>
+                                     <div class="skeleton-loader skeleton-family"><p></p></div>
+                                     </div>`
+    }
+
+    const response = await fetch(url);
+  
+    const jsonResult = await response.json();
+  
+    console.log(jsonResult);
+  
+    const characters = jsonResult;
+
+    const filteredCharacters = characters.filter(function(character){
+      return character.fullName.toLowerCase().includes(searchValue);
+    });
+
+    resultsContainer.innerHTML = ``;
+    
+    filteredCharacters.forEach(function(character){
+      resultsContainer.innerHTML += `<a href="../details.html?id=${character.id}" class="card">
+                                     <div class="image" style="background-image: url(${character.imageUrl});" alt="picture of Game of Thrones character"></div>
+                                     <div class="details">
+                                       <h4 class="fullname">${character.fullName}</h4>
+                                       <h4 class="family">${character.family}</h4>
+                                     </div>
+                                     </a>`;
+    });
+    console.log(resultsContainer);
+  } catch (e){
+    resultsContainer.innerHTML = errorMessage;
+    console.log(e);
+  };
+};
--- src/js/details.js
+++ src/js/details.js
@@ -44,3 +44,56 @@ Helper functions
  * @example: https://github.com/S3ak/fed-javascript1-api-calls/blob/main/src/js/detail.js#L36
  * @param {item} item The object with properties from the fetched JSON data.
  */
+
+import errorMessage from "./components/error.js";
+
+const characterContainer = document.querySelector(".details-container");
+const changeHeader = document.querySelector("#js-h1");
+
+characterContainer.innerHTML += `<div class="skeleton-loader skeleton-image-details" style="background-image: url('')"></div>`
+                                 
+
+
+const queryString = document.location.search;
+
+console.log(queryString);
+
+const params = new URLSearchParams(queryString);
+
+const id = params.get("id");
+
+console.log(id);
+
+const url = "https://thronesapi.com/api/v2/Characters/" + id ;
+
+
+async function fetchCharacter() {
+  try {
+    const detailsResponse = await fetch(url);
+    const detailsResult = await detailsResponse.json();
+
+    console.log(detailsResult);
+
+    newHtml(detailsResult);
+
+    changeHeader.innerHTML = detailsResult.fullName;
+    document.title = `Details for ${detailsResult.fullName}`;
+  } catch(e) {
+    characterContainer.innerHTML = errorMessage;
+    console.log(e);
+  }
+}
+
+fetchCharacter();
+
+function newHtml(detailsResult) {
+  characterContainer.innerHTML = `
+                                <div class="character-parent-container">
+                                <div class="character-image-container">
+                                <div class="character-image" style="background-image: url('${detailsResult.imageUrl}')" alt="Picture of Game of Thrones character"></div>
+                                </div>
+                                <h2 class="character-family">${detailsResult.family}</h2>
+                                <h2 class="character-title">${detailsResult.title}</h2>
+                                </div>
+                                `
+};
